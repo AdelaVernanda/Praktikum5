84ad31e552471c24604e1cfa4c6640857961a1ef
AdelaVernanda <115912073+AdelaVernanda@users.noreply.github.com>
2022-11-26 07:48:25 +0700

    Praktikum5

---
+++ Manager.java
@@ -0,0 +1,32 @@
+public class Manager extends Pegawai{
+    private double tunjangan;
+
+    public void setTunjangan(double tunjangan){
+        this.tunjangan = tunjangan; 
+    }
+
+    public double getTunjangan(){
+        return tunjangan;
+    }
+   
+
+    public Manager(String nama){
+        super(nama);
+    }
+    
+    public Manager(String nama, double gajiPokok){
+        super(nama, gajiPokok);
+    }
+
+    public Manager(String nama, double gajiPokok, double tunjangan){
+        super(nama, gajiPokok);
+        this.tunjangan = tunjangan;
+    }
+
+    
+    public void cetakInfo() {
+        super.cetakInfo();
+        System.out.println ("tunjangan : " + getTunjangan());
+    }
+}
+
